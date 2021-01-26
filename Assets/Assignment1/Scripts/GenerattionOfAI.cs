40ef6062529fe2bf5ec5d213097a3db5b118c383
ReubenXuereb <56310716+ReubenXuereb@users.noreply.github.com>
2021-01-26 14:34:00 +0100

    Task2 done

---
+++ Assets/Assignment1/Scripts/GenerattionOfAI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerattionOfAI : MonoBehaviour
+{
+
+
+
+    public List<Vector3> allObstaclePositions;
+    [SerializeField] GameObject AI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        generateTenAI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Vector3 randomGenerator()
+    {
+        Vector3 positions;
+        int x, y;
+        do
+        {
+            x = Random.Range(-49, 49);
+            y = Random.Range(-49, 49);
+            positions = new Vector3(x, y);
+        }
+        while (allObstaclePositions.Contains(positions));
+
+        return positions;
+    }
+
+    public void generateTenAI()
+    {
+        for (int i = 0; i <= 9; i++)
+        {
+            Vector3 positions = randomGenerator();
+            allObstaclePositions.Add(positions);
+            GameObject obstacles = Instantiate(AI, positions, Quaternion.identity);
+        }
+    }
+}
